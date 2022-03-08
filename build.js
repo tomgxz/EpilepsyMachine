c5932fe9e55b413b7f1e3d2553f964524bd1597a
Tomgxz <thomaslee10.TL@gmail.com>
2022-03-08 13:37:28 +0000

    Gradient Flicker

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
 function timeout() {
   setTimeout(function() {
-    document.body.style.backgroundColor="rgb(x,y,z)".replace("x",Math.floor(Math.random()*255)).replace("y",Math.floor(Math.random()*255)).replace("z",Math.floor(Math.random()*255))
+    color1="rgb(x,y,z)".replace("x",Math.floor(Math.random()*255)).replace("y",Math.floor(Math.random()*255)).replace("z",Math.floor(Math.random()*255))
+    color2="rgb(x,y,z)".replace("x",Math.floor(Math.random()*255)).replace("y",Math.floor(Math.random()*255)).replace("z",Math.floor(Math.random()*255))
+    if (Math.random() > 0.2) {
+      document.body.style.backgroundImage="linear-gradient(zdeg,x,y)".replace("x",color1).replace("y",color2).replace("z",Math.floor(Math.random()*360))
+    } else {
+      document.body.style.backgroundImage="linear-gradient(zdeg,x,y)".replace("x",color1).replace("y",color1).replace("z",0)
+    }
     timeout()
   },100);
 }
