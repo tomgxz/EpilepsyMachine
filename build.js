59b3ab69ef83100550e786f79b0fd8c44a2c4226
Tomgxz <thomaslee10.TL@gmail.com>
2023-06-15 17:28:52 +0100

    Update build.js

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ function timeout() {
   setTimeout(function() {
     color1="rgb(x,y,z)".replace("x",Math.floor(Math.random()*255)).replace("y",Math.floor(Math.random()*255)).replace("z",Math.floor(Math.random()*255))
     color2="rgb(x,y,z)".replace("x",Math.floor(Math.random()*255)).replace("y",Math.floor(Math.random()*255)).replace("z",Math.floor(Math.random()*255))
+    color2=color1
     if (Math.random() > 0.2) {
       document.body.style.backgroundImage="linear-gradient(zdeg,x,y)".replace("x",color1).replace("y",color2).replace("z",Math.floor(Math.random()*360))
     } else {
